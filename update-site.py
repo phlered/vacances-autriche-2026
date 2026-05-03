bfe47354c6562cef966b4fd5265873e9eb377788
Philippe Comte <phlered@laposte.net>
2026-05-03 12:55:19 +0200

    Fix markdown-to-HTML converter to properly handle iframes

--- update-site.py
+++ update-site.py
@@ -27,19 +27,27 @@ if not head_match:
 head_part = head_match.group(1)
 tail_part = '\n</body>\n</html>'
 
-# Convert markdown to HTML while preserving iframes
+# Convert markdown to HTML while preserving HTML blocks
 html_content = md_content
 
-# Protect HTML content (iframes, HTML blocks)
+# Protect HTML content (iframes, all HTML tags)
 protected = {}
 counter = 0
 
-# Protect iframes
-for match in re.finditer(r'<iframe[^>]*>.*?</iframe>', html_content, re.DOTALL):
-    key = f'___PROTECTED_{counter}___'
-    protected[key] = match.group(0)
-    html_content = html_content.replace(match.group(0), key, 1)
-    counter += 1
+# Protect iframes and any HTML tags (they shouldn't be converted)
+def protect_html(text):
+    global counter
+    def replace_match(match):
+        global counter
+        key = f'___PROTECTED_{counter}___'
+        protected[key] = match.group(0)
+        counter += 1
+        return key
+    # Match any HTML tags including iframes
+    text = re.sub(r'<[^>]+(?:>.*?</[^>]+>|/>)', replace_match, text, flags=re.DOTALL)
+    return text
+
+html_content = protect_html(html_content)
 
 # Simple markdown conversion
 html_content = re.sub(r'^### (.*?)$', r'<h3>\1</h3>', html_content, flags=re.MULTILINE)
@@ -51,7 +59,7 @@ html_content = re.sub(r'\[(.*?)\]\((.*?)\)', r'<a href="\2">\1</a>', html_conten
 html_content = re.sub(r'!\[(.*?)\]\((.*?)\)', r'<img src="\2" alt="\1">', html_content)
 html_content = re.sub(r'^\* (.*?)$', r'<li>\1</li>', html_content, flags=re.MULTILINE)
 
-# Restore protected content
+# Restore protected content BEFORE wrapping in paragraphs
 for key, value in protected.items():
     html_content = html_content.replace(key, value)
 
